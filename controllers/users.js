8241149e13130cf29ddce6b97107712597c2282c
Jessica Im <me@jessicaim.com>
2021-11-05 10:13:39 -0400

    saving in changes from branching

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const User = require('../models/users.js')
 
@@ -8,40 +7,40 @@ const User = require('../models/users.js')
 //      res.send('hihihi')
 // })
 
-// create user
-router.post('/', (req, res) => {
-     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-     User.create(req.body, (err, newUser) => {
-          res.json(newUser)
-     })
-})
-
-// show all users
 router.get('/', (req, res) => {
-     User.find({}, (err, allUsers) => {
-          res.json(allUsers)
-     })
+     res.json('Hello World')
 })
 
-// show single user
-router.get('/:id', (req, res) => {
-     User.findById(req.params.id, (err, foundUser) => {
-          res.json(foundUser)
+router.post('/createaccount', (req, res) => {
+     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+     User.create(req.body, (err, createdUser) => {
+          if(err){
+               console.log(err);
+               res.json(err.message)
+          } else {
+               console.log('user is created', createdUser);
+               res.json(createdUser)
+          }
      })
-})
+});
 
-// edit
-router.put('/:id', (req, res) => {
-     User.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedUser) => {
-          res.json(updatedUser)
-     })
-})
 
-// delete
-router.delete('/:id', (req, res) => {
-     User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
-          res.json(deletedUser)
+router.put('/login', (req, res) => {
+     console.log(req.body);
+     User.findOne({username: req.body.username}, (err, foundUser) => {
+          if(err) {
+               res.json('Oops, there was an error. Please try again')
+          } else {
+               if(!foundUser){
+                    res.json('Username and password do not match. Please try again.')
+               } else if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+                    res.json({username: foundUser.username})
+               } else {
+                    res.json('Username and password do not match. Please try again.')
+               }
+          }
      })
-})
+});
+
 
 module.exports = router;
