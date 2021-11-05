a6a92c6389073fe69921323b089b920bdd9b382a
Jessica Im <me@jessicaim.com>
2021-11-04 21:20:38 -0400

    saving in working users controller

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,43 +1,43 @@
-// const express = require('express')
-// const bcrypt = require('bcrypt')
-// const router = express.Router()
-// const User = require('../models/users.js')
-//
-// router.get('/', (req, res) => {
-//      if (req.session.currentUser) {
-//           User.findById(req.session.currentUser._id, (err, foundUser) => {
-//                if (err) {
-//                     console.log(err);
-//                }
-//                res.json(foundUser)
-//           })
-//      } else {
-//           res.json(req.session)
-//      }
-// })
-//
-//
-// router.post('/', (req, res) => {
-//      User.findOne({username: req.body.username}, (err, foundUser) => {
-//           if (err) {
-//                res.json({err: "oops the db had a problem" })
-//           } else if (!foundUser) {
-//                res.json({err: "no user found"})
-//           } else {
-//                if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-//                     req.session.currentUser = foundUser
-//                     res.json(foundUser)
-//                } else {
-//                     res.json({err: "password does not match"})
-//                }
-//           }
-//      })
-// })
-//
-// router.delete('/', (req, res) => {
-//      req.session.destroy(() => {
-//           res.json("session logged out")
-//      })
-// })
-//
-// module.exports = router;
+const express = require('express')
+const bcrypt = require('bcrypt')
+const router = express.Router()
+const User = require('../models/users.js')
+
+router.get('/', (req, res) => {
+     if (req.session.currentUser) {
+          User.findById(req.session.currentUser._id, (err, foundUser) => {
+               if (err) {
+                    console.log(err);
+               }
+               res.json(foundUser)
+          })
+     } else {
+          res.json(req.session)
+     }
+})
+
+
+router.post('/', (req, res) => {
+     User.findOne({username: req.body.username}, (err, foundUser) => {
+          if (err) {
+               res.json({err: "oops the db had a problem" })
+          } else if (!foundUser) {
+               res.json({err: "no user found"})
+          } else {
+               if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+                    req.session.currentUser = foundUser
+                    res.json(foundUser)
+               } else {
+                    res.json({err: "password does not match"})
+               }
+          }
+     })
+})
+
+router.delete('/', (req, res) => {
+     req.session.destroy(() => {
+          res.json("session logged out")
+     })
+})
+
+module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -1,70 +1,47 @@
-// const express = require('express')
-// const mongoose = require('mongoose')
-// const router = express.Router()
-// const bcrypt = require('bcrypt')
-// const User = require('../models/users.js')
-//
-// router.post('/', (req, res) => {
-//      req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
-//      User.create(req.body, (err, newUser) => {
-//           res.json(newUser)
-//      })
-// })
-//
-// router.get('/', (req, res) => {
-//      User.find({}, (err, allUsers) => {
-//           res.json(allUsers)
-//      })
-// })
-//
-
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const User = require('../models/users.js')
 
 // router.get('/', (req, res) => {
 //      res.send('hihihi')
 // })
-//
-// router.get('/', (req, res) => {
-//      User.find({}, (err, allUsers) => {
-//           res.json(allUsers)
-//      })
-// })
-//
-// router.post('/', (req, res) => {
-//      User.create(req.body, (err, newUser) => {
-//           res.json(newUser)
-//      })
-// })
-//
-// router.delete('/:id', (req, res) => {
-//      User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
-//           res.json(deletedUser)
-//      })
-// })
-//
-// router.put('/:id', (req, res) => {
-//      User.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedUser) => {
-//           res.json(updatedUser)
-//      })
-// })
-//
-// // Deleting Author Deletes Associated Articles
-// router.delete('/:id', (req, res) => {
-//      User.findByIdAndRemove(req.params.id, (err, foundUser) => {
-//           const entryIds = []
-//           for (let i = 0; i < foundUser.articles.length; i++) {
-//                entryIds.push(foundUser.entries[i]._id)
-//           }
-//           Entry.remove(
-//                {
-//                     _id: {
-//                          $in: entryIds
-//                     }
-//                },
-//                (err, data) => {
-//                     res.json(foundUser)
-//                }
-//           )
-//      })
-// })
 
-// module.exports = router;
+// create user
+router.post('/', (req, res) => {
+     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+     User.create(req.body, (err, newUser) => {
+          res.json(newUser)
+     })
+})
+
+// show all users
+router.get('/', (req, res) => {
+     User.find({}, (err, allUsers) => {
+          res.json(allUsers)
+     })
+})
+
+// show single user
+router.get('/:id', (req, res) => {
+     User.findById(req.params.id, (err, foundUser) => {
+          res.json(foundUser)
+     })
+})
+
+// edit
+router.put('/:id', (req, res) => {
+     User.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedUser) => {
+          res.json(updatedUser)
+     })
+})
+
+// delete
+router.delete('/:id', (req, res) => {
+     User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
+          res.json(deletedUser)
+     })
+})
+
+module.exports = router;
